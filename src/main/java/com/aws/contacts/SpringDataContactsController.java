8aea13626df93a40e286d73b66352559ec2fc737
coder983 <coder983@protonmail.com>
2020-04-28 18:11:42 -0400

    Redesigned Main View by removing the Delete Contact Button and Update Contact Button and adding them as links for individual contacts. The Delete link now works

--- src/main/java/com/aws/contacts/SpringDataContactsController.java
+++ src/main/java/com/aws/contacts/SpringDataContactsController.java
@@ -9,9 +9,7 @@ import com.aws.contacts.service.ContactServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 /**
@@ -51,6 +49,14 @@ public class SpringDataContactsController {
 		
 		contactRepo.save(contact);
 		service.ListContacts(model);
+		return "mainview";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String deleteContact(@PathVariable("id") long id, ModelMap model){
+		Contact contact = contactRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid student Id: " + id));
+		contactRepo.delete(contact);
+		model.addAttribute("contacts", contactRepo.findAll());
 		return "mainview";
 	}
 
